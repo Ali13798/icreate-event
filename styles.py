d41c525cc7d8c98504a3ff9e7a60a4ba622a7a1c
Ali K <karimiafsharali@gmail.com>
2022-10-16 11:52:16 -0500

    Extract style constants into a separate file

--- styles.py
+++ styles.py
@@ -1,31 +1,31 @@
-class GameStyles:
-    """Includes the style specifications of the AGV COntrol GUI"""
+import constants
 
-    FONT_SIZE = 20
-    PADDING = 20
+
+class GameStyles:
+    """Includes the style specifications of the GUI"""
 
     @staticmethod
     def config_styles(style) -> None:
         style.configure(
             "TButton",
-            background="#fcc200",
-            font=(None, GameStyles.FONT_SIZE),
-            padding=GameStyles.PADDING,
+            background=constants.LABEL_BG_COLOR,
+            font=(None, constants.ELEMENT_FONT_SIZE),
+            padding=constants.ELEMENT_PADDING,
         )
 
         # Configure styles - Labels
         style.configure(
             "TLabel",
-            font=(None, GameStyles.FONT_SIZE),
-            # background="#5e0009",
-            background="#fcc200",
+            font=(None, constants.ELEMENT_FONT_SIZE),
+            background=constants.LABEL_BG_COLOR,
             foreground="black",
-            padding=GameStyles.PADDING,
+            padding=constants.ELEMENT_PADDING,
         )
 
         # Configure styles - Frames
         style.configure(
             "TFrame",
-            background="#5e0009",
+            background=constants.FRAME_BG_COLOR,
         )
+
         return style
