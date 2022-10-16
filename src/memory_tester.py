def6f7bb0c25f97d36c8de785fe5c0e1bef8160f
Ali K <karimiafsharali@gmail.com>
2022-10-16 17:57:56 -0500

    Split sensor labels into separate pane

--- src/memory_tester.py
+++ src/memory_tester.py
@@ -16,25 +16,46 @@ class GUI(ttk.Frame):
         self.master.columnconfigure(0, weight=1)
         self.grid(sticky=tk.NSEW)
 
-        for i in range(3):
-            self.rowconfigure(i, weight=1)
-            self.columnconfigure(i, weight=1)
+        self.rowconfigure(1, weight=1)
+        self.columnconfigure(0, weight=1)
 
         self.labels: list[ttk.Label] = []
 
         self.style = ttk.Style()
         self.style = GameStyles.config_styles(self.style)
 
-        self.create_labels_grid()
+        self.create_panes()
+        self.grid_panes()
 
-    def create_labels_grid(self):
+        self.populate_sensors_pane()
+
+    def create_panes(self) -> None:
+        """Creates the different panes and store them as instance variables"""
+
+        self.sensors_pane = ttk.Frame(self)
+        self.info_pane = ttk.Frame(self)
+
+    def grid_panes(self):
+        self.info_pane.grid(sticky=tk.NSEW)
+        self.sensors_pane.grid(sticky=tk.NSEW)
+
+        for i in range(3):
+            self.sensors_pane.rowconfigure(i, weight=1)
+            self.info_pane.rowconfigure(i, weight=1)
+            self.sensors_pane.columnconfigure(i, weight=1)
+            self.info_pane.columnconfigure(i, weight=1)
+
+    def populate_sensors_pane(self):
         for row in range(3):
             for col in range(3):
                 sensor_id = col + row * 3 + 1
                 lbl_name = f"Sensor {sensor_id}"
                 style_name = f"S{sensor_id}.TLabel"
                 lbl = ttk.Label(
-                    self, text=lbl_name, style=style_name, anchor=tk.CENTER
+                    self.sensors_pane,
+                    text=lbl_name,
+                    style=style_name,
+                    anchor=tk.CENTER,
                 )
                 padding_value = 3
                 lbl.grid(
